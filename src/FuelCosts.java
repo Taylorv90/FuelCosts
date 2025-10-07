1ecd1fe2ca9081bfdbcfa07f829898fedd10691d
vtaylor <taylorv9@mail.uc.edu>
2025-10-06 20:33:02 -0400

    .

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -1,15 +1,97 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+public class FuelCosts
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double tankSize = 0.0;
+        double mpg = 0.0;
+        double pricePerGal = 0.0;
+        double miles100cost = 0.0;
+        double fullTankDistance = 0.0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        boolean done = false;
+        String trash = " ";
+
+        do
+        {
+            System.out.print("Enter tank size: ");
+            if(in.hasNextDouble())
+            {
+                tankSize = in.nextDouble();
+                in.nextLine();
+                if(tankSize <= 0)
+                {
+                    System.out.println("Tank size needs to be greater than 0");
+                }else
+                {
+                    done = true;
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Illegal number: " + trash + " please entre a valid input");
+            }
+        }while(!done);
+
+        done = false;
+
+        do
+        {
+            System.out.print("Enter the miles per gallon: ");
+            if(in.hasNextDouble())
+            {
+                mpg = in.nextDouble();
+                in.nextLine();
+                if(mpg <= 0)
+                {
+                    System.out.println("Miles per gallon needs to be greater than 0");
+                }else
+                {
+                    done = true;
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Illegal number: " + trash + " please entre a valid input");
+            }
+        }while(!done);
+
+        done = false;
+
+        do
+        {
+            System.out.print("Enter the price per gallon: ");
+            if(in.hasNextDouble())
+            {
+                pricePerGal = in.nextDouble();
+                in.nextLine();
+                if(pricePerGal <= 0)
+                {
+                    System.out.println("Price per gal needs to be greater than 0");
+                }else
+                {
+                    done = true;
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Illegal number: " + trash + " please entre a valid input");
+            }
+        }while(!done);
+
+        //  double miles100cost = 0.0;
+        //  double fullTankDistance = 0.0;
+
+        miles100cost = (100/mpg)*pricePerGal;
+        fullTankDistance = tankSize * mpg;
+
+        System.out.printf("The cost to drive 100 miles is $%.2f\n", miles100cost);
+        System.out.printf("The distance that can be driven on a full tank is " + fullTankDistance);
+
     }
 }
